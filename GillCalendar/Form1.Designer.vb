e31588b837628e09864d797185c8be3ae27645d0
Nate <nathanschnitzer587@gmail.com>
2019-01-16 11:47:12 -0500

    Initial Commit

---
+++ GillCalendar/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.dtpDate = New System.Windows.Forms.DateTimePicker()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'dtpDate
+        '
+        Me.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpDate.Location = New System.Drawing.Point(125, 312)
+        Me.dtpDate.Name = "dtpDate"
+        Me.dtpDate.Size = New System.Drawing.Size(200, 20)
+        Me.dtpDate.TabIndex = 0
+        Me.dtpDate.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(474, 433)
+        Me.Controls.Add(Me.dtpDate)
+        Me.Name = "Form1"
+        Me.Text = "Calendar"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dtpDate As DateTimePicker
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GillCalendar/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Dim sngCount As Single
+    Dim strDate As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sngCount = 0
+        strDate = dtpDate.Value.Second
+        Me.BackgroundImage = GillCalendar.My.Resources.Gill1
+        My.Computer.Audio.Play(My.Resources.kalinkawav, AudioPlayMode.BackgroundLoop)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If strDate.Equals(dtpDate.Value.Second) = False Then
+            sngCount = sngCount + 1
+            If sngCount Mod 21 = 0 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill1
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 1 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill2
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 2 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill3
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 3 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill4
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 4 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill5
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 5 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill6
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 6 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill7
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 7 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill8
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 8 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill9
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 9 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill10
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 10 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill11
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 11 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill12
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 12 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill13
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 13 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill14
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 14 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill15
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 15 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill16
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 16 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill17
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 17 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill18
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 18 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill19
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 19 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill20
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            ElseIf sngCount Mod 21 = 20 Then
+                Me.BackgroundImage = GillCalendar.My.Resources.Gill21
+                Me.BackgroundImageLayout = BackgroundImageLayout.Stretch
+            End If
+            Timer1.Interval = Timer1.Interval * 0.993
+        End If
+    End Sub
+End Class
